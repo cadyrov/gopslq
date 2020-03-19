49d52e60775e05429e35c4db5fe2e39d295b8c7b
kot <cadyrov@gmail.com>
2020-03-19 19:06:09 +0500

    init

--- builder.go
+++ builder.go
@@ -1,8 +1,12 @@
 package gopsql
 
+import "fmt"
+
 type Builder struct {
 	sqlString string
 	params    []interface{}
+	limit     int
+	offset    int
 }
 
 func NewB() *Builder {
@@ -15,9 +19,25 @@ func (b *Builder) Add(statement string, args ...interface{}) {
 }
 
 func (b *Builder) RawSql() string {
-	return b.sqlString
+	sql := b.sqlString
+	if b.limit > 0 {
+		sql += fmt.Sprintf(" LIMIT %d", b.limit)
+	}
+	if b.offset > 0 {
+		sql += fmt.Sprintf(" OFFSET %d", b.offset)
+	}
+	return sql
 }
 
 func (b *Builder) Values() []interface{} {
 	return b.params
+}
+
+func (b *Builder) Pagination(limit int, offset int) {
+	if limit >= 0 {
+		b.limit = limit
+	}
+	if offset >= 0 {
+		b.offset = offset
+	}
 }

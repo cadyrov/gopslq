9b6cf1a7d619a4b1112f7c0683068d819ac3a902
rugingetcat.com <cadyrov@gmail.com>
2020-06-12 14:37:36 +0500

    Update config.go

--- config.go
+++ config.go
@@ -1,13 +1,13 @@
 package gopsql
 
 type Config struct {
-	Host           string `json:"host"`
-	Port           int    `json:"port"`
-	UserName       string `json:"userName"`
-	DbName         string `json:"dbName"`
-	Password       string `json:"password"`
-	SslMode        string `json:"sslMode"`
-	Binary         bool   `json:"binary"`
-	MaxConnections int    `json:"maxConnections"`
-	ConnectionIdle int    `json:"connectionIdle"`
+	Host           string `json:"host" yaml:"host"`
+	Port           int    `json:"port" yaml:"port"`
+	UserName       string `json:"userName" yaml:"userName"`
+	DbName         string `json:"dbName" yaml:"dbName"`
+	Password       string `json:"password" yaml:"password"`
+	SslMode        string `json:"sslMode" yaml:"sslMode"`
+	Binary         bool   `json:"binary" yaml:"binary"`
+	MaxConnections int    `json:"maxConnections" yaml:"maxConnections"`
+	ConnectionIdle int    `json:"connectionIdle" yaml:"connectionIdle"`
 }
